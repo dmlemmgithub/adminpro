12718eb85f66378160c4cc47577a8b393bd25a0c
David <david.lmd84@gmail.com>
2018-01-19 16:32:49 +0100

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,13 @@
+//MODULOS
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {PagesModule} from './pages/pages.module';
+import {SharedModule} from './shared/shared.module';
 //RUTAS
 import { APP_ROUTES } from './app-routes';
 //COMPONENTES
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './/shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { RegisterComponent } from './login/register/register.component';
 import { PagesComponent } from './pages/pages.component';
 
@@ -22,19 +17,14 @@ import { PagesComponent } from './pages/pages.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
     RegisterComponent,
     PagesComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTES
+    PagesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
